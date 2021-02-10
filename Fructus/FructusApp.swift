e26c2ec91f0ea028bc74494ff88de0f069846e8a
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-02-10 00:03:39 -0300

    first commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Marcos Barbosa on 10/02/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
