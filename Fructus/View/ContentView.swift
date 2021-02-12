752606d60740f27d60040ea88c4e418210196de0
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-02-11 21:12:37 -0300

    Create @AppStorage variable, to control when appears OnboardingView or ContentView;

---
+++ Fructus/View/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by Marcos Barbosa on 11/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
