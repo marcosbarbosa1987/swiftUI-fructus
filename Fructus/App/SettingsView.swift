2390f8696dcbc3c529a77048cbfbff69e7250902
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-02-13 02:08:22 -0300

    Start create third section in SettingsView;

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -12,6 +12,7 @@ struct SettingsView: View {
     // MARK: - Properties
     
     @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
     
     // MARK: - Body
     
@@ -40,6 +41,23 @@ struct SettingsView: View {
                     }
                     
                     // MARK: - Section Two
+                    
+                    GroupBox (
+                    label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")
+                    ) {
+                        Divider().padding(.vertical, 8)
+                        
+                        Text("If you wish, you can restart the application by toggle the switch in this box. That way it starts the onboarding process and you will see the welcome screen again.")
+                            .padding(.vertical, 8)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                        
+                        Toggle(isOn: $isOnboarding, label: {
+                            Text("Restart".uppercased())
+                        })
+                    }
                     
                     // MARK: - Section Three
                     
